8e00f68ac539eee95f856c1bb4ae96b095af97ac
marchuka <anastasia.v.marchuk@gmail.com>
2024-11-29 09:17:59 +0100

    changed one single word in main

--- src/daneel/__main__.py
+++ src/daneel/__main__.py
@@ -28,7 +28,7 @@ def main():
     parser.add_argument(
         "-a",
         "--atmosphere",
-        dest="complete",
+        dest="astmosphere",
         required=False,
         help="Atmospheric Characterisazion from input transmission spectrum",
         action="store_true",
